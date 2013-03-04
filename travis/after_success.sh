445be6da244d36b825d6e96c610be1a7d944c243
Manuel Alabor <msites@msites.net>
2013-03-04 15:37:42 +0100

    travis instructions

---
+++ travis/after_success.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo '######################################'
+echo '#            AFTER SUCCESS           #'
+echo '#              - START -             #'
+echo '######################################'
+
+echo '---- Cloning repo into /tmp ----'
+cd /tmp
+git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} gh-pages 2>&1
+cd gh-pages
+
+echo '---- Switch to gh-pages branch ----'
+git checkout gh-pages
+
+echo '---- Copy latest PDF build ----'
+cp $RESULT_PDF_PATH ./
+
+echo '---- Set git settings ----'
+git config --global user.name $GIT_AUTHOR_NAME
+git config --global user.email $GIT_AUTHOR_EMAIL
+
+echo '---- Add files, commit and push ----'
+git add -A
+git commit -m "adding latest build pdf to gh-pages"
+git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
+
+echo '######################################'
+echo '#            AFTER SUCCESS           #'
+echo '#            - FINISHED -            #'
+echo '######################################'
---
+++ travis/before_install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo '######################################'
+echo '#           BEFORE_INSTALL           #'
+echo '#              - START -             #'
+echo '######################################'
+
+echo '---- Cleaning Temp Dir ----'
+cd /tmp
+rm -fr *
+
+echo '---- Download and unzip install-tl ----'
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar xvfz install-tl-unx.tar.gz
+cd `ls -d */`
+
+echo '---- Get install-tl profile ----'
+wget https://raw.github.com/mweibel/BA-Dokumentation/master/travis/texlive-installation.profile
+
+echo '---- Install texlive with install-tl ----'
+sudo ./install-tl --profile=texlive-installation.profile
+
+echo '---- Install tikz-uml ----'
+cd /tmp
+wget http://www.ensta-paristech.fr/~kielbasi/tikzuml/src/tikzuml-v1.0b-2013-02-01.tbz
+tar xvfz tikzuml-v1.0b-2013-02-01.tbz
+sudo mv tikzuml-v1.0b /usr/local/texlive/2012/texmf/tex/latex/tikzuml
+sudo texhash /usr/local/texlive/2012/texmf
+
+echo '######################################'
+echo '#           BEFORE_INSTALL           #'
+echo '#            - FINISHED -            #'
+echo '######################################'
