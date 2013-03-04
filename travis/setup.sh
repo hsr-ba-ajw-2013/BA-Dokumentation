301fb08a253b581c554c3cbbca81edcefff9e337
Manuel Alabor <msites@msites.net>
2013-03-04 17:29:20 +0100

    fixed setup.sh

--- travis/setup.sh
+++ travis/setup.sh
@@ -5,7 +5,7 @@ echo '#              - START -             #'
 echo '######################################'
 
 PATH=$PATH:/usr/local/texlive/2012/bin/i386-linux
-../make
+make
 
 echo '######################################'
 echo '#                SETUP               #'
